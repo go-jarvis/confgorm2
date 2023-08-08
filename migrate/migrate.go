fffe917fda35341d1516cd43d5c5a7cb76a52d9e
tangx <uyinn28@gmail.com>
2023-08-08 11:39:51 +0800

    feat: add migrate intial

--- migrate/migrate.go
+++ migrate/migrate.go
@@ -1,16 +1,22 @@
 // migrate 提供一个临时存储自动创建或更新表的仓库。
-// 这段代码不应该被引用。 只是作为案例片段。
-// 应该在自己的代码中创建不会冲突的仓库。
 package migrate
 
 import "gorm.io/gorm"
 
-var bucket = make([]any, 0)
-
-func AppendTables(tables ...any) {
-	bucket = append(bucket, tables...)
+type bucket struct {
+	tables []any
 }
 
-func Migrate(migrator gorm.Migrator) error {
-	return migrator.AutoMigrate(bucket...)
+func New() *bucket {
+	return &bucket{
+		tables: make([]any, 0),
+	}
+}
+
+func (b *bucket) AddTable(tables ...any) {
+	b.tables = append(b.tables, tables...)
+}
+
+func (b *bucket) Migrate(m gorm.Migrator) error {
+	return m.AutoMigrate(b.tables...)
 }
